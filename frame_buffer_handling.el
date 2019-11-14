f682caf324fad87e473036adafcb966a66f0b6c1
Per Karlstrom <per@hudson-trading.com>
2019-11-14 11:16:24 -0500

    no need fir packet init in e 27

--- frame_buffer_handling.el
+++ frame_buffer_handling.el
@@ -22,3 +22,12 @@
 ;;;(require 'framemove)
 ;;;(windmove-default-keybindings)
 ;;;(setq framemove-hook-into-windmove t)
+
+;;Show paranthesis
+(setq show-paren-delay 0)           ; how long to wait?
+(show-paren-mode t)                 ; turn paren-mode on
+(setq show-paren-style 'expression) ; alternatives are 'parenthesis' and 'mixed'
+;;;(set-face-background 'show-paren-match-face "#000033")
+;;;(set-face-attribute 'show-paren-match-face nil
+;;;        :weight 'bold :underline nil :overline nil :slant 'normal)
+
