14c73d75ceb2dbc979bb15d3c6e99d1a37b5eba2
Per Karlstrom <per@hudson-trading.com>
2019-11-14 11:16:24 -0500

    no need fir packet init in e 27

--- packages.el
+++ packages.el
@@ -2,15 +2,3 @@
 
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
-(package-initialize)
-
-;;Show paranthesis
-(setq show-paren-delay 0)           ; how long to wait?
-(show-paren-mode t)                 ; turn paren-mode on
-(setq show-paren-style 'expression) ; alternatives are 'parenthesis' and 'mixed'
-;;;(set-face-background 'show-paren-match-face "#000033")
-;;;(set-face-attribute 'show-paren-match-face nil
-;;;        :weight 'bold :underline nil :overline nil :slant 'normal)
-
-
