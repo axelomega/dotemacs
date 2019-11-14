1c6c8478358d17908c186d1f1b4ce9b72a346b5f
Per Karlström <per@karlstrom.se>
2019-11-14 01:04:43 -0500

    Start to use helm

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; If there is more than one, they won't work right.
  '(initial-buffer-choice 'remember-notes)
  '(package-selected-packages
-   '(helm-make helm-projectile helm-tramp helm-git-grep helm-google magit helm dash dash-docs projectile projectile-codesearch)))
+   '(helm-swoop helm-eww helm-taskswitch helm-flyspell helm-ls-git helm-youtube helm-spotify helm-spotify-plus helm-make helm-projectile helm-tramp helm-git-grep helm-google magit helm dash dash-docs projectile projectile-codesearch)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
