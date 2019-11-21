7a9207bfc837998c5a787e37fa64f60d9519a8df
Per Karlstrom <per@hudson-trading.com>
2019-11-20 22:30:53 -0500

    move more to settings

--- projectile.el
+++ projectile.el
@@ -4,7 +4,7 @@
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (setq projectile-enable-caching t)
 (setq projectile-indexing-method 'hybrid)
-(setq projectile-generic-command "find . -type f -not -wholename '*rewritten/*' -not -wholename '*rewritten_xcelium/*' -not -wholename '*dse*/*' -not -wholename '*btrade/conf/*' -not -wholename '*/obj_dir/*' -print0")
-(setq projectile-git-command "find . -type f -not -wholename '*rewritten/*' -not -wholename '*rewritten_xcelium/*' -not -wholename '*dse*/*' -not -wholename '*btrade/conf/*' -not -wholename '*/obj_dir/*' -print0")
+(setq projectile-generic-command my-settings-projectile-generic-command)
+(setq projectile-git-command my-settings-projectile-generic-comman)
 (setq projectile-sort-order 'recently-active)
 
