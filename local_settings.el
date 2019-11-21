9a709d3c05259573488b037f0e83c68edc567509
Per Karlstrom <per@hudson-trading.com>
2019-11-20 22:34:10 -0500

    better template file

--- local_settings.el
+++ local_settings.el
@@ -2,7 +2,10 @@
 ;;; copy to ~/.emacs.d/
 
 ;;; ccls settings
-(setq my-settings-ccls-executable "<path to ccls executable>")
+;; (setq my-settings-ccls-executable "<path to ccls executable>")
 
 ;;; helm-jira settings
-(setq my-settings-jira-url        "url to JIRA project")
+;; (setq my-settings-jira-url        "url to JIRA project")
+
+;;; Projectile
+;; (setq my-settings-projectile-generic-command "find . -type f -not -wholename '*some_foler_to_filter/*' -not -wholename '*some_other_folder_to_filter/*' -print0")
