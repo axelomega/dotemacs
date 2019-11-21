06c6292ac88254b398cd134b676faad15468920e
Per Karlstrom <per@hudson-trading.com>
2019-11-20 21:57:05 -0500

    added some comments

---
+++ local_settings.el
@@ -0,0 +1,8 @@
+;;; host / project specific things.
+;;; copy to ~/.emacs.d/
+
+;;; ccls settings
+(setq my-settings-ccls-executable "<path to ccls executable>")
+
+;;; helm-jira settings
+(setq my-settings-jira-url        "url to JIRA project")
