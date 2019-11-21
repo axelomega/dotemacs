137757fdb041520351d743efb8f42ff99889f93c
Per Karlstrom <per@hudson-trading.com>
2019-11-20 21:57:05 -0500

    added some comments

--- init.el
+++ init.el
@@ -1,18 +1,20 @@
-(load "~/dotemacs/emacs_init.el")
-
+;;; symlink this file to ~/.emacs.d/init.el
+(load-file "~/.emacs.d/local_settings.el")
+(load-file "~/dotemacs/emacs_init.el")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(initial-buffer-choice 'remember-notes)
- '(minimap-dedicated-window nil)
- '(minimap-minimum-width 15)
- '(minimap-mode t)
- '(minimap-width-fraction 0.05)
+ '(company-minimum-prefix-length 2)
+ '(inhibit-startup-echo-area-message "per")
+ '(inhibit-startup-screen t)
+ '(initial-buffer-choice nil)
+ '(minimap-highlight-line nil)
+ '(minimap-width-fraction 0.1)
  '(minimap-window-location 'right)
  '(package-selected-packages
-   '(minimap sublimity helm-swoop helm-eww helm-taskswitch helm-flyspell helm-ls-git helm-youtube helm-spotify helm-spotify-plus helm-make helm-projectile helm-tramp helm-git-grep helm-google magit helm dash dash-docs projectile projectile-codesearch)))
+   '(json-mode json-navigator jsonnet-mode helm-company auto-package-update use-package helm-git-files helm-git-grep helm-ls-git ccls company-lsp helm-lsp ws-butler helm-jira magit minimap sml-modeline sml-mode helm helm-projectile helm-spotify-plus helm-swoop helm-tramp projectile elpy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
